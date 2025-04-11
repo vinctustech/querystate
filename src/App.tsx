ed537c09d37fa1ac3f0d98957aff0ccc1289ab21
Edward A Maxedon <edadma@gmail.com>
2025-04-11 10:43:21 -0400

    basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import {Button, Card} from "antd";
+import { Button, Card } from 'antd'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <Card>
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-      </Card>
+    <Card>
+      <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
+    </Card>
   )
 }
 
