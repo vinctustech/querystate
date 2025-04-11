8f456fd875909f91f9a2a9434c55d440d344f5c5
Edward A Maxedon <edadma@gmail.com>
2025-04-11 10:42:16 -0400

    basic setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import 'antd/dist/reset.css'
+import { ConfigProvider, Layout, theme } from 'antd'
+import { Content } from 'antd/es/layout/layout'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ConfigProvider
+      theme={{
+        algorithm: theme.darkAlgorithm,
+      }}
+    >
+      <Layout style={{ minHeight: '100vh', background: '#141414' }}>
+        <Content style={{ padding: '24px', background: '#141414' }}>
+          <App />
+        </Content>
+      </Layout>
+    </ConfigProvider>
   </StrictMode>,
 )
